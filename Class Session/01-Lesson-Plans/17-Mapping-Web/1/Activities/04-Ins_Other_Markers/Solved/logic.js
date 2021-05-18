4f943a27b4ddbc9c13521ff366747e7103cbc5bf
Ravi Patel <rp70960@gmail.com>
2021-05-18 14:40:41 -0400

    17th Week 1st

---
+++ Class Session/01-Lesson-Plans/17-Mapping-Web/1/Activities/04-Ins_Other_Markers/Solved/logic.js
@@ -0,0 +1,59 @@
+// Create an initial map object
+// Set the longitude, latitude, and the starting zoom level
+var myMap = L.map("map").setView([45.52, -122.67], 13);
+
+// Add a tile layer (the background map image) to our map
+// Use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Create a new marker
+L.marker([45.52, -122.67]).addTo(myMap);
+
+// Create a circle and pass in some initial options
+L.circle([45.52, -122.69], {
+  color: "green",
+  fillColor: "green",
+  fillOpacity: 0.75,
+  radius: 500
+}).addTo(myMap);
+
+// Create a Polygon and pass in some initial options
+L.polygon([
+  [45.54, -122.68],
+  [45.55, -122.68],
+  [45.55, -122.66]
+], {
+  color: "yellow",
+  fillColor: "yellow",
+  fillOpacity: 0.75
+}).addTo(myMap);
+
+// Coordinates for each point to be used in the polyline
+var line = [
+  [45.51, -122.68],
+  [45.50, -122.60],
+  [45.48, -122.70],
+  [45.54, -122.75]
+];
+
+// Create a polyline using the line coordinates and pass in some initial options
+L.polyline(line, {
+  color: "red"
+}).addTo(myMap);
+
+// Create a rectangle and pass in some initial options
+L.rectangle([
+  [45.55, -122.64],
+  [45.54, -122.61]
+], {
+  color: "black",
+  weight: 3,
+  stroke: true
+}).addTo(myMap);
---
+++ Class Session/01-Lesson-Plans/17-Mapping-Web/1/Activities/05-Stu_Other_Markers/Unsolved/logic.js
@@ -0,0 +1,22 @@
+// Create our initial map object
+// Set the longitude, latitude, and the starting zoom level
+var myMap = L.map("map").setView([39.8283, -98.5795], 5);
+
+// Add a tile layer (the background map image) to our map
+// Use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Create a red circle over Dallas
+
+
+// Connect a black line from NYC to Toronto
+
+
+// Create a purple polygon that covers the area in Atlanta, Savannah, Jacksonville and Montgomery
